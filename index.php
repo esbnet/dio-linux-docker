1619169f39b5ec873e3af1267533ef280cbb1e25
Edmilson Soares <esbnet@gmail.com>
2022-08-16 22:41:36 -0300

    create config files

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+<title>Exemplo PHP</title>
+</head>
+<body>
+
+<?php
+ini_set("display_errors", 1);
+header('Content-Type: text/html; charset=iso-8859-1');
+
+echo 'Versão Atual do PHP: ' . phpversion() . '<br>';
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "meubanco";
+
+// Criar conexão
+$link = new mysqli($servername, $username, $password, $database);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$valor_rand1 =  rand(1, 999);
+$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
+$host_name = gethostname();
+
+$query = "INSERT INTO dados (alunoId, nome, sobrenome, endereco, cidade, host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+
+if ($link->query($query) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $link->error;
+}
+
+?>
+</body>
+</html>
